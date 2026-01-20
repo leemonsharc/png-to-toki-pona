2019a9d17e05c88a24c3e49ff48ef23a10b9e56f
leemonsharc <146793021+leemonsharc@users.noreply.github.com>
2026-01-20 11:08:03 -0800

    Refactor upload.py to convert PNG to .tok file (toki pona)

--- upload.py
+++ upload.py
@@ -1,17 +1,25 @@
 import binascii
 
-toki_pona_wordlist = [
-    "li", "e", "mi", "ni",
-    "jan", "toki", "pona", "pi",
-    "la", "tawa", "lon", "ala",
-    "sina", "ona", "mute", "tenpo"
-]
 
+hex_to_toki = {
+    "0": "li", "1": "e", "2": "mi", "3": "ni",
+    "4": "jan", "5": "toki", "6": "pona", "7": "pi",
+    "8": "la", "9": "tawa", "a": "lon", "b": "ala",
+    "c": "sina", "d": "ona", "e": "mute", "f": "tenpo"
+}
+
+output = ""
 
 def main():
-    path = input("enter filename: ")
-    with open(path, 'rb') as f:
+    path = input("enter png filename without .png: ")
+    with open(path + ".png", 'rb') as f:
         png = f.read()
     tohex = binascii.hexlify(png).decode()
+    for letter in tohex:
+        global output
+        totoki = hex_to_toki[letter]
+        output += (totoki + " ")
+    with open(path + ".tok", "w") as f:
+        f.write(output)
 
 main()
